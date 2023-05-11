8db894f82f02ccf899056b6a09532566a589e3dc
Yan Carlos <yancarlos55@yahoo.com.br>
2023-05-11 10:34:01 -0300

    Redirecionamento para a index conluido provisoriamente

--- serverConfig.js
+++ serverConfig.js
@@ -1,10 +1,13 @@
 const http = require("http");
 const express = require("express");
 const app = express();
+//var home = require('./index.html')
 
 app.get("/", function (req, res) {
     console.log(req.url);
-    res.sendFile("/index.html");
+    res.sendFile('index.html', {
+        root: "./website"
+    })
 });
 
-http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));;
+http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"))
