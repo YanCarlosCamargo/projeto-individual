f0862dfd5c5a1b1b1f25171367fd0eb727c10b1d
Yan Carlos <yancarlos55@yahoo.com.br>
2023-05-25 20:08:56 -0300

    Sistema para mostrar usuarios na tela de feed pela aba lateral implementado

--- website/js/posts.js
+++ website/js/posts.js
@@ -61,7 +61,7 @@ function listarPost(listaPosts, listaLikes) {
                                     </div>
                                     <label class="tituloPost">${element.titulo}</label>
                                 </div>
-                                <label class="labelAutor" onclick="abrirUsuario(${element.fkAutor})">Por: ${element.apelido}</label>
+                                <label class="labelAutor" onclick="abrirUsuario(${element.fkAutor})">Ver mais de ${element.apelido}</label>
                                 <label class="descricaoPost">${element.descPost}</label>
                                 <div class="badgeLikesPost" onclick=${jaDeuLike ? ("removerLike(" + element.idPost + ")") : "inserirLike(" + element.idPost + ")"}>
                                 ${!jaDeuLike ? '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="red" d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3Z"/></svg>'
@@ -268,12 +268,39 @@ function ranking() {
         });
 }
 
-
+var ultimoId;
 async function abrirUsuario(idUsuario) {
     console.log("idUsuario", idUsuario);
     fetch(`/blog/buscarUsuario/${idUsuario}`)
         .then(response => response.json()).then(data => {
-            console.log("Perfil Recebido", data[0]);
-            labelNome.innerHTML = data[0].apelido; imgProfileLateral.style.backgroundImage = `url(${data[0].imgPerfil})`;
+            if (ultimoId != idUsuario) {
+                ultimoId = data[0].idUsuario;
+                console.log("Perfil Recebido", data[0]);
+                labelNome.innerHTML = data[0].apelido; imgProfileLateral.style.backgroundImage = `url(${data[0].imgPerfil})`;
+                containerAbaLateral.classList.remove('containerAbaLateral');
+                containerAbaLateral.classList.add('containerAbaLateralAberto');
+                wrapperConteudoLateral.classList.remove('wrapperConteudoLateral');
+                wrapperConteudoLateral.classList.add('wrapperConteudoLateralAberto');
+                setTimeout(() => {
+                    wrapperConteudoLateral.classList.remove('wrapperConteudoLateralAberto');
+                    wrapperConteudoLateral.classList.add('wrapperConteudoLateral');
+                    containerAbaLateral.classList.remove('containerAbaLateralAberto');
+                    containerAbaLateral.classList.add('containerAbaLateral');
+
+                }, 2000);
+            } else {
+                containerAbaLateral.classList.remove('containerAbaLateral');
+                containerAbaLateral.classList.add('containerAbaLateralAberto');
+                wrapperConteudoLateral.classList.remove('wrapperConteudoLateral');
+                wrapperConteudoLateral.classList.add('wrapperConteudoLateralAberto');
+                setTimeout(() => {
+                    wrapperConteudoLateral.classList.remove('wrapperConteudoLateralAberto');
+                    wrapperConteudoLateral.classList.add('wrapperConteudoLateral');
+                    containerAbaLateral.classList.remove('containerAbaLateralAberto');
+                    containerAbaLateral.classList.add('containerAbaLateral');
+
+                }, 2000);
+            }
         }).catch(console.log)
+
 }
